93fa8674adc80421be9cc3b69e5a743aec60fa7a
nickchenjin <58397177@qq.com>
2023-04-26 22:07:17 +0800

    init project

---
+++ cloud-api-commons/src/main/java/org/example/entities/TestGit.java
@@ -0,0 +1,17 @@
+package org.example.entities;
+
+import lombok.Data;
+
+/**
+ * Classname:TestGit
+ * Package:org.example.entities
+ * Description:
+ *
+ * @Author:NickChJ
+ * @Create:2023/4/26 - 22:06
+ * @Version: V1.0
+ */
+@Data
+public class TestGit {
+    private String testId;
+}
